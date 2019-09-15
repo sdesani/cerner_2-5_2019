64d3315bed065e13d698d3291a01a981be0f8457
sdesani <santosh.desani@gmail.com>
2019-09-15 15:20:30 -0500

    GO SimpleGetCall - [09/14]

---
+++ GO/SimpleGETCall.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+)
+
+func main() {
+	CallFHIREndpoint()
+}
+
+// Call to open FHIR endpoint using net/http
+
+func CallFHIREndpoint() {
+	resp, err := http.Get("https://fhir-open.sandboxcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Patient?_id=4342009")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+}
